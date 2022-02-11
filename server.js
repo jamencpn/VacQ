d290729b3916fc6e6d3a29dc6466038b2962d039
Natchapon <natchapon.p1997@gmail.com>
2022-02-11 15:50:24 +0700

    added models, connected with mongo

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const res = require('express/lib/response');
-const hospitals = require('./routes/hospitals')
+
+// Route files
+const hospitals = require('./routes/hospitals');
+
+const connectDB = require('./config/db');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
-const app=express();
+//Connect to database
+connectDB();
 
+//Body parser
+const app=express();
+app.use(express.json());
 app.use('/api/v1/hospitals',hospitals)
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandleRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
