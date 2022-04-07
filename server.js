25cf8be4a1ed48246c4f0df04849fbc9180c74c4
Natchapon <natchapon.p1997@gmail.com>
2022-04-07 21:43:55 +0700

    OpenAPI with swagger

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
 const cors = require('cors');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 //Rate Limiting
 const limiter=rateLimit({
     windowsMs:10*60*1000,
     max: 100
-})
+});
 
 // Route files
 const hospitals = require('./routes/hospitals');
@@ -29,11 +32,32 @@ connectDB();
 
 //Body parser
 const app=express();
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ]
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 app.use(express.json());
 app.use(cors()); // requests across domain
 app.use(helmet()); // header protection
 app.use(limiter); // limit requests max/time
-app.use(hpp()); // pram pollution
+app.use(hpp()); // param pollution
 app.use(mongoSanitize());  // Injection (input query {"$gt":""})
 app.use(xss()); // remove </script> input
 app.use('/api/v1/hospitals',hospitals);
